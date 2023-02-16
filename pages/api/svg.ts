945340c67893fd77b83b81c75407476155d9005e
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-02-16 12:42:19 +0700

    update: no-cache

--- pages/api/svg.ts
+++ pages/api/svg.ts
@@ -14,6 +14,6 @@ export default function handler(
   res.statusCode = 200;
   res.setHeader("Content-type", "image/svg+xml");
   res.setHeader("Cache-Control",
-    "no-store");
+    "no-cache");
   return res.end(svg);
 }
